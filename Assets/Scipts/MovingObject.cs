586231a463bdf08c68c5572c5b05823664a7973e
artman9036 <zacestsartis@gmail.com>
2026-07-23 00:52:10 +0100

    Allows ease in out interpolation between each node.

--- Assets/Scipts/MovingObject.cs
+++ Assets/Scipts/MovingObject.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 public class MovingObject : MonoBehaviour
 {
     public List<Transform> nodes = new List<Transform>();
+    public bool smoothInterpolation = false;
     public bool useTime = true;
     public float time = 1f;
     public float moveSpeed;
 
+    private float elapsedTime;
+    private int currentNode = 0;
     private int nextNode = 1;
     private void Start()
     {
@@ -27,12 +30,29 @@ public class MovingObject : MonoBehaviour
 
     private void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, nodes[nextNode].transform.position, moveSpeed*Time.deltaTime);
-        //(Vector2) before transform.position just forces it to be a vector2 value so the compiler doesnt scream at you
-        if ((Vector2)transform.position == (Vector2)nodes[nextNode].transform.position)
+        if (!smoothInterpolation)
         {
-            //This is also the formula you use for circle queues, which I think is pretty neat!
-            nextNode = (nextNode + 1) % nodes.Count;
+            transform.position = Vector2.MoveTowards(transform.position, nodes[nextNode].transform.position, moveSpeed * Time.deltaTime);
+            //(Vector2) before transform.position just forces it to be a vector2 value so the compiler doesnt scream at you
+            if ((Vector2)transform.position == (Vector2)nodes[nextNode].transform.position)
+            {
+                //This is also the formula you use for circle queues, which I think is pretty neat!
+                nextNode = (nextNode + 1) % nodes.Count;
+            }
+        }
+        else
+        {
+            elapsedTime += Time.deltaTime;
+            float percentageComplete = elapsedTime / (time/nodes.Count);
+
+            transform.position = Vector2.Lerp(nodes[currentNode].transform.position, nodes[nextNode].transform.position, Mathf.SmoothStep(0, 1, percentageComplete));
+            if ((Vector2)transform.position == (Vector2)nodes[nextNode].transform.position)
+            {
+                currentNode = nextNode;
+                nextNode = (nextNode + 1) % nodes.Count;
+                Debug.Log(elapsedTime);
+                elapsedTime = 0;
+            }
         }
 
     }

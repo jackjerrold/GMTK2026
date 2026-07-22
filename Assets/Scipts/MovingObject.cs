9ab2f63a2765fbe9dc3bcd88c82be2d03323524e
artman9036 <zacestsartis@gmail.com>
2026-07-22 23:45:33 +0100

    Allows path nodes, bool to use time or a specified movespeed
    Essentially, add empty objects into the list to create a looping path. The time is how long it will take for the program to go back to the starting position again.

--- Assets/Scipts/MovingObject.cs
+++ Assets/Scipts/MovingObject.cs
@@ -2,30 +2,37 @@ using UnityEngine;
 using System.Collections.Generic;
 public class MovingObject : MonoBehaviour
 {
-    public Vector2 startPosition = new Vector2(0, 0);
-    public Vector2 endPosition = new Vector2(0, 0);
+    public List<Transform> nodes = new List<Transform>();
+    public bool useTime = true;
     public float time = 1f;
+    public float moveSpeed;
 
-    private float moveSpeed;
-    private Vector2 nextPosition;
+    private int nextNode = 1;
     private void Start()
     {
-        transform.position = startPosition;
-        nextPosition = startPosition;
-        //Done this cause time feels easier to use than "movespeed"
-        moveSpeed = (endPosition - startPosition).magnitude / time;
+        transform.position = nodes[0].transform.position;
+
+        if (useTime)
+        {
+            //calculates the distance of all the nodes
+            float sumOfDistancesBetweenNodes = 0;
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                int next = (i + 1) % nodes.Count;
+                sumOfDistancesBetweenNodes += (nodes[next].transform.position - nodes[i].transform.position).magnitude;
+            }
+            moveSpeed = (sumOfDistancesBetweenNodes / time);
+        } 
     }
 
     private void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, nextPosition, moveSpeed*Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, nodes[nextNode].transform.position, moveSpeed*Time.deltaTime);
         //(Vector2) before transform.position just forces it to be a vector2 value so the compiler doesnt scream at you
-        if ((Vector2)transform.position == nextPosition)
+        if ((Vector2)transform.position == (Vector2)nodes[nextNode].transform.position)
         {
-            //weird operator bullshit i saw from a tutorial
-            //if the comparison is true, nextPosition will be startPosition
-            //if the comparison is false, it'll be endPosition
-            nextPosition = (nextPosition == endPosition) ? startPosition : endPosition;
+            //This is also the formula you use for circle queues, which I think is pretty neat!
+            nextNode = (nextNode + 1) % nodes.Count;
         }
 
     }

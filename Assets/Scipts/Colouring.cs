5d8f6e52a0571191570187e27a9de23fe32b6642
aSlicedOrange <terkahubert@gmail.com>
2026-07-23 19:08:46 +0100

    Added cloud and lightning absorbtion and collision detection and colouring file
    1: i added a cloud, and the ray comes from the cloud to check for collisions but i left the if statement empty cause idk if to use Destroy() or maybe a custom Break method for animations. (explained in comments in the file. i might js be stupid tho lmk)
    
    2: i re-added the canAbsorb bs cause i think i didnt save it and i made the lightning file change the isCharged attribute in rodControls and the isCharged runs out after a given duration (lots of words)
    
    3: new Colouring.cs file for js colouring player, lightning, cloud, rod, ground, and charged rod
    
    i think thats all i added today

---
+++ Assets/Scipts/Colouring.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Colouring : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer player;
+
+    [SerializeField]
+    private SpriteRenderer rod;
+
+    [SerializeField]
+    private RodControls rodScript;
+
+    [SerializeField]
+    private SpriteRenderer lightning;
+
+    [SerializeField]
+    private SpriteRenderer cloud;
+
+    [SerializeField]
+    private SpriteRenderer ground;
+
+    void Start()
+    {
+        player.color = new Color32(255, 255, 255, 255); //White
+        rod.color = new Color32(178, 76, 4, 255); //Copper-ish
+        lightning.color = new Color32(14, 191, 192, 255); //Light Blue
+        cloud.color = new Color32(83, 83, 91, 255); //Grey
+        ground.color = new Color32(5, 125, 13, 255); //Green
+    }
+
+    
+    void Update()
+    {
+        rod.color = (rodScript.IsCharged()) ? new Color32(14, 191, 192, 255) : new Color32(178, 76, 4, 255);
+    }
+}
--- Assets/Scipts/Lightning.cs
+++ Assets/Scipts/Lightning.cs
@@ -9,6 +9,19 @@ public class Lightning : MonoBehaviour
     private GameObject lightning;
 
     [SerializeField]
+    private Transform cloud;
+
+    [SerializeField]
+    private RodControls rod;
+
+
+    [SerializeField]
+    private LayerMask obstacleLayer; //Layer at which the obstacles will be (in Sprite Renderer)
+    //The player and rod is at 0 rn and the lightning is at -1 so its behind the player but thats temporary.
+    //I think the cloud and lightning will be set at some foreground value (especially cloud) so js lmk or tell the level design ppl or whatever idk
+
+
+    [SerializeField]
     private float countdown = 5f; //Holy Peak
 
     private float timer = 0f;
@@ -22,19 +35,57 @@ public class Lightning : MonoBehaviour
     {
         timer += Time.deltaTime;
         transform.position = new Vector2(player.position.x, player.position.y);
+        //cloud.position = new Vector2(player.position.x, player.position.y);
+
         /*
         Moves where the player x is and y
         The placement of the lightning folder thing changes
         The square inside it has a set y offset of 1.5 so the bottom of the lightning is with the bottom of player (temporary)
         */
 
-        if (timer >= countdown) { //Make the lightning appear when countdown is reached
-            lightning.SetActive(true);
-            timer = 0f;
-        } else if (timer >= 1.0f) {
-            lightning.SetActive(false); //Hide the lightning after x seconds
+
+        if (timer >= countdown) { 
+                RaycastHit2D ray = Raycast(player.position);
+                //ray.collider == null means no collision
+                //ray.collider != null means ray.collider has info on the object it collided with
+
+                lightning.SetActive(true); //Make the lightning appear when countdown is reached
+
+                //I tried to avoid big IF statement towers but i js cant here without having RAY not defined or not checking RAY before ROD.ABSORB()
+                //Rewrite if you can make it more readable (maybe class variable but idk)
+                if (ray.collider == null) { 
+                    if (rod.Absorb() == true) {
+                        lightning.SetActive(false); //Hides the lightning if the rod absorbed it
+                    }
+                } else {
+                    /*
+                    i got no code to write here cause idk how u want it to break
+                    (either js Destroy() or some Break() method for animations)
+                    but you can access the object through the RaycastHit2D ray variable above
+
+                    Destroy(ray.collider.gameObject);
+                    or
+                    ray.collider.gameObject.Break();
+                    .. or however it should look like
+                    */
+                }
+                timer = 0f;
+        } else if (timer >= 1.0f) { //Hide the lightning after x seconds
+            lightning.SetActive(false); 
         }
 
+    }
+
+    void FixedUpdate() {
         Debug.Log($"Timer: {timer}");
+    }
+
+    private RaycastHit2D Raycast(Vector2 targetPosition) {
+        Vector2 startPosition = new Vector2(cloud.position.x, cloud.position.y);
+        Vector2 distance = targetPosition - startPosition;
+        
+        RaycastHit2D ray = Physics2D.Raycast(startPosition, distance.normalized, distance.magnitude, obstacleLayer);
+
+        return ray;
     }
 }

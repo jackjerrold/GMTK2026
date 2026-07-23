a41ca86835abc423cae1146357558a144ee54f1d
artman9036 <zacestsartis@gmail.com>
2026-07-23 01:11:44 +0100

    Allows a wait time before moving to the next node.

--- Assets/Scipts/MovingObject.cs
+++ Assets/Scipts/MovingObject.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 public class MovingObject : MonoBehaviour
 {
     public List<Transform> nodes = new List<Transform>();
     public bool smoothInterpolation = false;
+    public float waitBetweenNodes = 0;
     public bool useTime = true;
     public float time = 1f;
     public float moveSpeed;
 
+    private bool shouldWait = false;
     private float elapsedTime;
     private int currentNode = 0;
     private int nextNode = 1;
@@ -30,30 +33,38 @@ public class MovingObject : MonoBehaviour
 
     private void Update()
     {
+        elapsedTime += Time.deltaTime;
+
+        if (shouldWait)
+        {
+            if (elapsedTime < waitBetweenNodes) { return; } else { elapsedTime = 0f; }
+        }
+
+        shouldWait = false;
+
         if (!smoothInterpolation)
         {
             transform.position = Vector2.MoveTowards(transform.position, nodes[nextNode].transform.position, moveSpeed * Time.deltaTime);
-            //(Vector2) before transform.position just forces it to be a vector2 value so the compiler doesnt scream at you
-            if ((Vector2)transform.position == (Vector2)nodes[nextNode].transform.position)
-            {
-                //This is also the formula you use for circle queues, which I think is pretty neat!
-                nextNode = (nextNode + 1) % nodes.Count;
-            }
         }
         else
         {
-            elapsedTime += Time.deltaTime;
-            float percentageComplete = elapsedTime / (time/nodes.Count);
-
+            float percentageComplete = elapsedTime / (time / nodes.Count);
             transform.position = Vector2.Lerp(nodes[currentNode].transform.position, nodes[nextNode].transform.position, Mathf.SmoothStep(0, 1, percentageComplete));
-            if ((Vector2)transform.position == (Vector2)nodes[nextNode].transform.position)
-            {
-                currentNode = nextNode;
-                nextNode = (nextNode + 1) % nodes.Count;
-                Debug.Log(elapsedTime);
-                elapsedTime = 0;
-            }
         }
+        //(Vector2) before transform.position just forces it to be a vector2 value so the compiler doesnt scream at you
+        if ((Vector2)transform.position == (Vector2)nodes[nextNode].transform.position)
+        {
+            currentNode = nextNode;
+            //This is also the formula you use for circle queues, which I think is pretty neat!
+            nextNode = (nextNode + 1) % nodes.Count;
+            Debug.Log(elapsedTime);
+            elapsedTime = 0;
 
+            if (waitBetweenNodes > 0)
+            {
+                shouldWait = true;
+            }
+
+        }
     }
 }

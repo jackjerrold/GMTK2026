1104c3cc63ee04611610b590125cca4a9c97db64
Space-Captain-Jeice <therealsongokuwu@gmail.com>
2026-07-25 23:48:24 +0100

    Title and settings fade in
    MainMenu and GameSettings now fade in from black (duration 1).

--- Assets/Scipts/SceneChangeButtonFade.cs
+++ Assets/Scipts/SceneChangeButtonFade.cs
@@ -1,24 +1,66 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
-public class SceneChangeButtonFade : MonoBehaviour
-{ 
-    // can pass a scene name to it from code
-    public void LoadSceneByName(string sceneName)
+public class SceneFader : MonoBehaviour
+{
+    // variables
+    public Image fadeImage;
+    public string sceneToLoad;
+    public float fadeDuration = 1f;
+
+
+    // runs at start of scene
+    private void Start()
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(FadeFromBlack());
     }
 
-    // can also pass scene build indices
-    public void LoadSceneByIndex(int sceneIndex)
+
+    // called by buttons that initiate scene change
+    public void FadeToScene()
     {
-        SceneManager.LoadScene(sceneIndex);
+        StartCoroutine(FadeOutAndLoad());
     }
-    public void QuitGame()
+
+
+    // fade to black and scene change
+    IEnumerator FadeOutAndLoad()
     {
-        Application.Quit();
-        #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-        #endif
+        float t = 0;
+        Color c = fadeImage.color;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            c.a = t / fadeDuration;
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+
+    // fade from black into scene
+    IEnumerator FadeFromBlack()
+    {
+        float t = 0;
+        Color c = fadeImage.color;
+
+        c.a = 1;
+        fadeImage.color = c;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            c.a = 1 - (t / fadeDuration);
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        c.a = 0;
+        fadeImage.color = c;
     }
 }
